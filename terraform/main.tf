b21f399d7123e6453971aca8d5a2de2a0a6ab12e
Saravanan Pazhanichami <71633998+sara-paz@users.noreply.github.com>
2024-07-04 16:59:15 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,8 +10,8 @@ terraform {
 # Configure the AWS Provider
 provider "aws" {
   region = "eu-west-2"
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
+  access_key = ${env.TF_ENV_AWS_ACCESS_KEY}
+  secret_key = ${env.TF_ENV_AWS_SECRET_KEY}
 }
 
 
