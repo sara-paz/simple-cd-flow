928c4a1bdacfadf6519d85125b75bed11393ef50
Saravanan Pazhanichami <71633998+sara-paz@users.noreply.github.com>
2024-07-04 17:21:37 +0100

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,12 +1,3 @@
-variable "TF_ENV_AWS_ACCESS_KEY" {
-  type        = string
-  description = "aws access key"
-}
-
-variable "TF_ENV_AWS_ACCESS_KEY" {
-  type        = string
-  description = "aws secret key"    
-}
 
 variable "aws_ec2_name" {
     type = string
