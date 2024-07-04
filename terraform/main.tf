0f63f5c9fc54e39a11ff47d044ba749e95e30e77
saravanan <saravanan.pazhanichamy@accionlabs.com>
2024-07-04 19:44:16 +0100

    fix region

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,7 +9,7 @@ terraform {
 
 # Configure the AWS Provider
 provider "aws" {
-  region = "eu-west-2"
+  region = "eu-north-1"
   access_key = var.AWS_ACCESS_KEY
   secret_key = var.AWS_SECRET_KEY
 }
